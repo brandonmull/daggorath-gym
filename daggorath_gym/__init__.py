77c23c489b14eada9b9d6eac866eec34ccda8980
brandonmull <brandonmull@users.noreply.github.com>
2026-08-04 16:13:40 -0400

    daggorath_gym: limit public API to DaggorathEnv

--- daggorath_gym/__init__.py
+++ daggorath_gym/__init__.py
@@ -1,9 +1,1 @@
-from .paths import GYM_PATH, ROOT_PATH, EMU_PATH
-from .config import *
-from .bridge import MameBridge
-
-# Make these paths available when importing the package
-__all__ = ['config', 'MameBridge', 'GYM_PATH', 'EMU_PATH', 'ROOT_PATH']
-
-# Daggorath Gym Environment
 from .env import DaggorathEnv
